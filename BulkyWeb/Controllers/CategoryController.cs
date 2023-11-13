cf2aedff6f7e5c3f5926f2c9b6a5784a26c9a576
Nada Sulaiman <nsulaiman@ejadacairo.local>
2023-11-13 17:41:16 +0200

    Implement CRUD for Category

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,126 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categoryList = _context.Categories.ToList();
+            return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Category name cannot match display order.");
+            }
+            if (category.Name != null && category.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Category name cannot be test");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                TempData["success"] = "The category has been added successfully";
+                return RedirectToAction("Index", "Category");
+
+            }
+            else
+            {
+                return View();
+            }
+           
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Category name cannot match display order.");
+            }
+            if (category.Name != null && category.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Category name cannot be test");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                TempData["success"] = "The category has been updated successfully";
+                return RedirectToAction("Index", "Category");
+
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            TempData["success"] = "The category has been deleted successfully";
+            return RedirectToAction("Index", "Category");
+
+
+
+
+        }
+    }
+}
